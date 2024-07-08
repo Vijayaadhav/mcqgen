733cd8f133a65513765e6b9e06f67c3f29eada8c
Vijayaadhav <vijayaadhav2396@gmail.com>
2024-07-08 20:04:40 +0530

    Final changes made

--- Streamlitapp.py
+++ Streamlitapp.py
@@ -3,11 +3,11 @@ import json
 import traceback
 import pandas as pd
 from dotenv import load_dotenv
-from mcqgenrator.utils import read_file,get_table_data
+from src.mcqgenerator.utils import read_file,get_table_data
 import streamlit as st
 from langchain.callbacks import get_openai_callback
-from mcqgenrator.MCQGenrator import generate_evaluate_chain
-from mcqgenrator.logger import logging
+from src.mcqgenerator.MCQGenerator import generate_evaluate_chain
+from src.mcqgenerator.logger import logging
 
 #loading json file
 
